41e8adbad46609812183123d4265e74a76081989
Mohamed Afzal Nandolia <afzal.nandolia@gmail.com>
2023-08-06 20:53:26 +0530

    updated code

--- Arrays & Hashing/containsDuplicate.java
+++ Arrays & Hashing/containsDuplicate.java
@@ -1,13 +1,14 @@
 import java.util.HashSet;
+//to check distinct elements, you can use HashSet, which can tell if any integer is repeated
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
 
         for(int i = 0; i < nums.length; i++){
-            if(set.contains(nums[i])) return true;
+            if(set.contains(nums[i])) return true;//if duplicate exists then return true
 
-            else set.add(nums[i]);
+            else set.add(nums[i]);//else add it to the set
         }
-        return false;
+        return false;//else return false
     }
 }
