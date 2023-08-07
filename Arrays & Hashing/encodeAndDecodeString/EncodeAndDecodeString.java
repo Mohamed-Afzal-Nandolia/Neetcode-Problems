4754d2446c52db2a6ebcc5d8380169f3cdc90e0c
Mohamed Afzal Nandolia <afzal.nandolia@gmail.com>
2023-08-07 17:38:20 +0530

    EncodeAndDecodeString

---
+++ Arrays & Hashing/encodeAndDecodeString/EncodeAndDecodeString.java
@@ -0,0 +1,25 @@
+package encodeAndDecodeString;
+
+public class EncodeAndDecodeString {
+    public static String encode (List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+
+        for (String str : strs) {
+            sb.append(str.length()).append("#").append(str); // 4#Iint
+        }
+        return sb.toString();
+    }
+    public static List<String> decode(String str) {
+        List<String> res = new ArrayList<>();
+        int i = 0;
+        while (i < str.length()) {
+
+            int delimeter = str.indexOf('#', i);
+            int length = Integer.parseInt(str.substring(i, delimeter));
+            String decoderStr = str.substring(delimeter + 1, delimeter + 1 + length);
+            res.add(decoderStr);
+            i = delimeter + 1 + length;
+        }
+        return res;
+    }
+}
